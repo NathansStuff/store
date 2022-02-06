9f1d0fa434d6f0d037ea562344543a0d21863f86
NathansStuff <n.odonnell@hotmail.com>
2022-02-06 11:21:56 +1100

    react router dom

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
-import products from '../products';
 import { Row, Col } from 'react-bootstrap';
+import products from '../products';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const HomeScreen = () => {
+  return (
+    <Fragment>
+      <h1>Product </h1>
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
