f837aeba40fc421334b7b98aae2fad4f55d04a1f
NathansStuff <n.odonnell@hotmail.com>
2022-02-15 18:31:52 +1100

    user login reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,21 +7,28 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReduces';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initalState = {
   cart: {
     cartItems: cartItemsFromStorage,
   },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
