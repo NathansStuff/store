9419860a27c3a49e4149399629799eaac1076bd8
NathansStuff <n.odonnell@hotmail.com>
2022-02-16 12:11:02 +1100

    shipping screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/login' element={<LoginScreen />} exact />
+            <Route path='/shipping' element={<ShippingScreen />} exact />
             <Route path='/profile' element={<ProfileScreen />} exact />
             <Route path='/register' element={<RegisterScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -41,7 +41,7 @@ const CartScreen = () => {
   };
 
   const checkoutHandler = () => {
-    navigate('/login?redirect=shipping');
+    navigate('/shipping');
   };
   return (
     <Row>
